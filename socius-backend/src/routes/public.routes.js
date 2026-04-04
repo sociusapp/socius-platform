f4f42a5827a6efeae32c50aa0c8dad8be2f0ad03
momtajhusen <mumtaz666raza@gmail.com>
2026-04-04 08:21:17 +0545

    fix: use authenticate middleware correctly in public routes

--- socius-backend/src/routes/public.routes.js
+++ socius-backend/src/routes/public.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const publicController = require('../controllers/public.controller');
-const authMiddleware = require('../middlewares/auth');
+const { authenticate } = require('../middlewares/auth');
 
 // Public route to render the capture page
 router.get('/capture', publicController.renderCapturePage);
@@ -10,9 +10,9 @@ router.get('/capture', publicController.renderCapturePage);
 router.post('/save-location', publicController.saveLocation);
 
 // Admin route to get all locations
-router.get('/all-locations', authMiddleware, publicController.getAllLocations);
+router.get('/all-locations', authenticate, publicController.getAllLocations);
 
 // Admin route to get visitor details
-router.get('/visitor/:visitorId', authMiddleware, publicController.getLocationByVisitorId);
+router.get('/visitor/:visitorId', authenticate, publicController.getLocationByVisitorId);
 
 module.exports = router;
